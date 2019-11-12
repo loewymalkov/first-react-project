eb5133a54de1ae7e0692b2b00f535bfcdfbf45ce
Student <example@example.com>
2019-11-12 11:29:19 -0800

    add css modules

--- webpack.config.js
+++ webpack.config.js
@@ -39,9 +39,9 @@ module.exports = {
         options: {
           emitWarning: true,
           configFile: "./.eslintrc.json"
-        }
-      },
-      {
+          }
+        },
+        {
         test: /\.jsx?$/,
         loader: "babel-loader",
         exclude: /node_modules/,
@@ -51,7 +51,8 @@ module.exports = {
             "react",
           ],
           plugins: [
-            "react-hot-loader/babel"
+            "react-hot-loader/babel",
+            "styled-jsx/babel"
           ]
         }
       }
