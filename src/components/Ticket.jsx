d504c28e3ae97f20406402a46b943d2abb8f9f1c
Student <example@example.com>
2019-11-12 10:46:21 -0800

    add linting config

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 function Ticket(props){
   return (
@@ -12,9 +12,9 @@ function Ticket(props){
 }
 
 Ticket.propTypes = {
-    names: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    issue: PropTypes.string
+  names: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  issue: PropTypes.string
 };
 
 export default Ticket;
